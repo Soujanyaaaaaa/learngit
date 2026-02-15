76cac4a60c867f5bc8f5cde0d9486727124183d6
Soujanyaaaaaa <soujnadiger99@gmail.com>
2026-02-15 22:33:15 +0530

    asjkg

--- ksdbk/src/test/java/ksdbk/ksnlsd.java
+++ ksdbk/src/test/java/ksdbk/ksnlsd.java
@@ -4,7 +4,7 @@ public class ksnlsd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-driver.findElement(SOujahnya).click();
+aklhslkskgdriver.findElement(SOujahnya).click();
 driver.findElement(Abhishek).click();
 driver.findElement(Suresh).click();
 driver.findElement(secondBranch).click();
