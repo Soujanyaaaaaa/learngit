635fca5e5924334a78f053ebd7743f9e034d5b26
Soujanyaaaaaa <soujnadiger99@gmail.com>
2026-02-15 22:25:20 +0530

    ksal

--- ksdbk/src/test/java/ksdbk/ksnlsd.java
+++ ksdbk/src/test/java/ksdbk/ksnlsd.java
@@ -21,14 +21,13 @@ Soujanya
 
 
 
-<<<<<<< HEAD
 public static void main1(String[]args) {
 akhshlasowl	
 SOujanya;
-=======
+}
 public static3 void main1(String[]args) {
 	kslSOujanya;
->>>>>>> c83ce6f07ec9b9fcd4c898bce452be36ee2d3a7b
+
 	Parimala;
 }
 }
